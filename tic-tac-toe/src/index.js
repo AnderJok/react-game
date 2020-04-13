245df0d496adc99f599bae6f1e18e99d237e03f4
AnderJok <ander.aranguren.c@gmail.com>
2020-04-13 11:54:12 +0100

    Add time travel

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -20,7 +20,6 @@ class Board extends React.Component {
       );
     }  
     render() {
-  
       return (
         <div>
           <div className="board-row">
@@ -50,12 +49,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext:true,
     }
   }  
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]){
@@ -66,26 +66,34 @@ class Game extends React.Component {
       // concat() doesn't mutate the original array, push() does.
       history: history.concat([{
         squares:squares,
-      }]),   
+      }]),
+        stepNumber: history.length,
         xIsNext:!this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber:step,
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
     
-    // const moves = history.map((step,move) => {
-    //   const desc = move ?
-    //     'Go to move #' + move :
-    //     'Go to game start';
-    //   return (
-    //     <li>
-    //       <button onClick={() => this.jumpTo(move)}>{desc}</button>
-    //     </li>
-    //   )
-    // });
+    const moves = history.map((step,move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    });
     
     let status;
     if (winner) {
