9b082472f4dc87efc85ec1915f17f5e46e6fc5df
Ander <ander.aranguren.c@gmail.com>
2020-10-19 16:47:25 +0200

    Adding a custom button component

---
+++ gitTests/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ButtonComponent = props = {
+	return (
+		<View>
+			<Text>{props.buttonName}</Text>
+		</View>
+	);
+};
+
+export default ButtonComponent;
